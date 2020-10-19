ccae412bcc70a7a0ae29d71dd93b4a8dc543210e
Andone <wlsdn110@gmail.com>
2020-10-19 23:36:55 +0900

    chatting layout finish

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -12,74 +12,70 @@
         height="500"
         item-height="64"
       >
-        <v-list two-line>
-          <template v-for="(item, index) in items.slice(0, 6)">
-            <v-subheader
-              v-if="item.header"
-              :key="item.header"
-            >
-              {{ item.header }}
-            </v-subheader>
-            <v-divider
-              v-else-if="item.divider"
-              :key="index"
-              :inset="item.inset"
-            ></v-divider>
-            <v-list-item
-              v-else
-              :key="item.title"
-            >
-              <v-list-item-content>
-                <v-list-item-title v-html="item.title"></v-list-item-title>
-                <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-        </v-list>
-        </v-virtual-scroll>
+        <template v-slot="{ item }">
+          <v-list-item :key="item">
+            <v-list-item-ID>
+              <v-list-item-title>
+                <strong>{{ item.ID + " : "}} </strong>
+                {{item.content}}
+              </v-list-item-title>
+            </v-list-item-ID>
+          </v-list-item>
+          <v-divider></v-divider>
+        </template>
+      </v-virtual-scroll>
       </v-card>
-      <form>
+      <v-form ref = "form" v-model="valid" @submit.prevent="submit">
         <v-text-field
         v-model="chat"
         label="chat"
-        required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
+        id="chat"
         ></v-text-field>
         <v-btn
         class="mr-4"
         @click="submit"
+        color="success"
         >
         submit
         </v-btn>
-        <v-btn @click="clear">
-        clear
+        
+        <v-btn @click="reset" color="warning">
+            clear
         </v-btn>
-     </form>
+     </v-form>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import axios from "axios"
+import io from "socket.io-client"
   export default {
-    data: () => ({
-      items: [
-        { header: 'Today' },
-        { avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', title: 'Brunch this weekend?', subtitle: "<span class='font-weight-bold'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-        { divider: true, inset: true },
-        { avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='font-weight-bold'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend." },
-        { divider: true, inset: true },
-        { avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg', title: 'Oui oui', subtitle: "<span class='font-weight-bold'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?" },
-      ],
-    }),
-    methods: {
-      submit () {
-        this.$v.$touch()
-      },
-      clear () {
-        this.$v.$reset()
-        this.chat = ''
-      },
+    data(){
+      return{
+        message: [],
+        socket:io('localhost:3001')
+      }
     },
+    methods:{
+        reset(){
+            this.$refs.form.reset()  
+        },
+        submit(){
+            console.log(this.chat);
+            axios({
+                method:'POST',
+                url : 'http://localhost:3001/message',
+                data:{
+                    message:this.chat    
+                },
+            });          
+        },
+        mount(){
+            this.socket.on('message',(data)=>{
+                this.message = [...this.message,data];
+            });
+        }
+    }
   }
 </script>
